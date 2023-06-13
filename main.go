39dcafcd17120d035c8595ac49f9a442f9bee4f2
Roronoa Zoro <thedonxrules@gmail.com>
2023-06-13 14:32:35 +0530

    test branch

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Testing Github-Actions")
+	log.Println("Server started....")
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Github"))
+	})
+
+	if err := http.ListenAndServe(":6565", r); err != nil {
+		log.Fatal(err)
+	}
 }
